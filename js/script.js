169e5fd9ffa9da5553674d188c30b0eb245aa62c
neu-astha <neuastha12@gmail.com>
2022-12-12 12:07:48 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+gsap.registerPlugin( DrawSVGPlugin);
+
+
+
+
+if (document.querySelector(".side-nav")) {
+    let menuBtn = document.querySelector(" .menu");
+    let page = document.querySelector(".nav_hub");
+
+    menuBtn.addEventListener('click', ()=> {
+        page.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+    })
+}
+
+
+
+var swiper = new Swiper(".slider", {
+    loop: true,
+    autoplay: 5500,
+    spaceBetween: 20,
+    breakpoints: {
+        1024: {
+            slidesPerView: 6,
+        },
+        400: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+
+gsap.from(".svgName", { drawSVG: 0, duration: 4, ease: "Linear.easeNone", repeat: 1, repeatDelay: 8 });
+
+gsap.from(".drawMe", { drawSVG: 0, duration: 6, ease: "Linear.easeNone", repeat: -1,});
